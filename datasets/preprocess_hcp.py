b476f11351383e52c7a52aa48355dff40a04a401
ExtremeViscent <zhangyiqi55732@sina.com>
2022-05-23 16:09:58 +0100

    misc

--- datasets/preprocess_hcp.py
+++ datasets/preprocess_hcp.py
@@ -46,6 +46,14 @@ def _load(x):
     image_t2 = sitk.GetArrayFromImage(image_t2)
     gt_t1 = sitk.GetArrayFromImage(gt_t1)
     gt_t2 = sitk.GetArrayFromImage(gt_t2)
+    image_t1 = (image_t1 - np.mean(image_t1)) / np.std(image_t1)
+    image_t2 = (image_t2 - np.mean(image_t2)) / np.std(image_t2)
+    gt_t1 = (gt_t1 - np.mean(gt_t1)) / np.std(gt_t1)
+    gt_t2 = (gt_t2 - np.mean(gt_t2)) / np.std(gt_t2)
+    image_t1 = image_t1.astype(np.half)
+    image_t2 = image_t2.astype(np.half)
+    gt_t1 = gt_t1.astype(np.half)
+    gt_t2 = gt_t2.astype(np.half)
     if not op.exists(preprocessed_path):
         os.makedirs(preprocessed_path)
     with h5.File(op.join(preprocessed_path, basename + '.h5'), 'w') as f:
@@ -54,7 +62,7 @@ def _load(x):
         f.create_dataset('gt_t1', data=gt_t1)
         f.create_dataset('gt_t2', data=gt_t2)
 def load(list_images_t1,list_images_t2,list_basenames):
-    thread_map(_load, zip(list_images_t1, list_images_t2,list_basenames), max_workers=1, total=num_samples)
+    thread_map(_load, zip(list_images_t1, list_images_t2,list_basenames), max_workers=32, total=num_samples)
 
 data_dir = '/scratch/users/k21113539/HCP_1200'
 list_dir = glob.glob(op.join(data_dir, '*'))
@@ -63,20 +71,21 @@ list_basenames = [op.basename(x) for x in list_dir]
 list_images_t1 = [op.join(x,'unprocessed','3T','T1w_MPR1',op.basename(x)+'_3T_T1w_MPR1.nii.gz') for x in list_dir]
 list_images_t2 = [op.join(x,'unprocessed','3T','T2w_SPC1',op.basename(x)+'_3T_T2w_SPC1.nii.gz') for x in list_dir]
 
-list_dir = list_dir[:300]
-list_basenames = list_basenames[:300]
-list_images_t1 = list_images_t1[:300]
-list_images_t2 = list_images_t2[:300]
+# list_dir = list_dir[:300]
+# list_basenames = list_basenames[:300]
+# list_images_t1 = list_images_t1[:300]
+# list_images_t2 = list_images_t2[:300]
 
 num_samples = len(list_dir)
 spacing = [1.0,1.0,1.0]
 spacing = np.array(spacing)
-spacing *= 6
-target_shape = (320, 320, 256)
+spacing *= 8
+target_shape = (108, 145, 145)
 factor = spacing[2] / spacing[0]
 aniso_transform = tio.RandomAnisotropy(axes=2, downsampling=factor)
 resize_transform = tio.Resize(target_shape=target_shape)
 resample_transform = tio.Resample(target=spacing)
-transform  = tio.Compose([resample_transform,resize_transform])
+blur_transform = tio.RandomBlur(6)
+transform  = tio.Compose([resample_transform,resize_transform,blur_transform])
 transform_gt = resize_transform
 load(list_images_t1,list_images_t2,list_basenames)
