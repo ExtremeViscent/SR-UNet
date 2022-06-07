07ce38352741cb82b036bf0bd2cb4d56e245871e
ExtremeViscent <zhangyiqi55732@sina.com>
2022-06-07 18:28:31 +0100

    updated config

--- configs/config_vae_hcp_t1_800.py
+++ configs/config_vae_hcp_t1_800.py
@@ -1,8 +1,8 @@
 from colossalai.amp import AMP_TYPE
 import os
 
-DATA_DIR="/scratch/users/k21113539/HCP_1200"
-OUTPUT_DIR="/scratch/users/k21113539/SR-UNet/experiments/output_vae_hcp_t1_800"
+DATA_DIR="/scratch/prj/bayunet/HCP_1200"
+OUTPUT_DIR="/scratch/prj/bayunet/experiments/output_vae_hcp_t1_800"
 AUGMENTATION=False
 INPUT_MODALITIES=["t1"]
 OUTPUT_MODALITIES=["t1"]
@@ -21,15 +21,15 @@ ALPHA = 0.000025
 DOWN_FACTOR = 5
 DATASET='HCP'
 
-NNODES=1
-NGPUS_PER_NODE=4
-WORLD_SIZE=NNODES*NGPUS_PER_NODE
+# NNODES=1
+# NGPUS_PER_NODE=4
+# WORLD_SIZE=NNODES*NGPUS_PER_NODE
 
-fp16=dict(
-    mode=AMP_TYPE.TORCH,
-)
+# fp16=dict(
+#     mode=AMP_TYPE.TORCH,
+# )
 
-parallel = dict(
-    data=dict(size=WORLD_SIZE),
-    tensor=dict(mode='1d', size=WORLD_SIZE),
-)
+# parallel = dict(
+#     data=dict(size=WORLD_SIZE),
+#     tensor=dict(mode='1d', size=WORLD_SIZE),
+# )
