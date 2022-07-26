64a066e16174398e08d32c4372619ad87b37d483
ExtremeViscent <limitviscent@gmail.com>
2022-07-26 17:57:25 +0100

    misc

--- configs/archived/config_vae_dhcp_dbg.py
+++ configs/archived/config_vae_dhcp_dbg.py
@@ -2,29 +2,24 @@ from colossalai.amp import AMP_TYPE
 import os
 
 DATA_DIR="/media/hdd/dhcp/dhcp_lores"
-OUTPUT_DIR="/media/hdd/viscent/SR-UNet/experiments/output_unet_dhcp_dbg"
+OUTPUT_DIR="/media/hdd/viscent/SR-UNet/experiments/output_vae_dhcp_dbg"
 AUGMENTATION=False
 INPUT_MODALITIES=["t1"]
 OUTPUT_MODALITIES=["t1"]
 BATCH_SIZE=3
 IN_CHANNELS=1
 OUT_CHANNELS=1
-NUM_EPOCHS=800
+NUM_EPOCHS=1000
 N_SPLITS=5
 F_MAPS=[16, 32, 64, 128, 256]
-VAE = True
 
-WARMUP_EPOCHS=2
+WARMUP_EPOCHS=None
 LR=0.001
-NUM_SAMPLES=50
-LATENT_SIZE = F_MAPS[-1]
-ALPHA = 0.000025
+NUM_SAMPLES=None
+LATENT_SIZE = 1
+ALPHA = 0.00025
 DOWN_FACTOR = 5
 DATASET='dHCP'
-
-NNODES=1
-NGPUS_PER_NODE=1
-WORLD_SIZE=NNODES*NGPUS_PER_NODE
 
 # fp16=dict(
 #     mode=AMP_TYPE.TORCH
