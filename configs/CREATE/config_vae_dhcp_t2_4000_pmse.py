a70c915968322734f3ac2ccd2bebd7983db08988
ExtremeViscent <zhangyiqi55732@sina.com>
2022-10-28 17:22:32 +0100

    more configs

--- configs/CREATE/config_vae_dhcp_t2_4000_pmse.py
+++ configs/CREATE/config_vae_dhcp_t2_4000_pmse.py
@@ -1,8 +1,8 @@
 from colossalai.amp import AMP_TYPE
 import os
 
-DATA_DIR = "/scratch/prj/bayunet/HCP_1200"
-OUTPUT_DIR = "/scratch/prj/bayunet/experiments/output_unet_hcp_t2_4000"
+DATA_DIR = "/scratch/prj/bayunet/dhcp_lores"
+OUTPUT_DIR = "/scratch/prj/bayunet/experiments/output_vae_dhcp_t2_4000_pmse"
 AUGMENTATION=False
 INPUT_MODALITIES=["t2"]
 OUTPUT_MODALITIES=["t2"]
@@ -13,22 +13,24 @@ NUM_EPOCHS=4000
 N_SPLITS=5
 F_MAPS=[16, 32, 64, 128, 256]
 
-VAE = False
-
 WARMUP_EPOCHS=None
 LR=0.001
 NUM_SAMPLES=None
-LATENT_SIZE = 8
+LATENT_SIZE = 256
 ALPHA = 0.00025
 DOWN_FACTOR = 5
-DATASET='HCP'
+DATASET='dHCP'
+AUGMENTATION=False
+
+DIV_LOSS = 'sinkhorn'
+RECON_LOSS = 'patch_mse'
 
 # NNODES=1
 # NGPUS_PER_NODE=4
 # WORLD_SIZE=NNODES*NGPUS_PER_NODE
 
 # fp16=dict(
-#     mode=AMP_TYPE.TORCH,
+#     mode=AMP_TYPE.TORCH
 # )
 
 # parallel = dict(
--- configs/CREATE/config_vae_hcp_t1_4000_pmse.py
+++ configs/CREATE/config_vae_hcp_t1_4000_pmse.py
@@ -2,18 +2,16 @@ from colossalai.amp import AMP_TYPE
 import os
 
 DATA_DIR = "/scratch/prj/bayunet/HCP_1200"
-OUTPUT_DIR = "/scratch/prj/bayunet/experiments/output_unet_hcp_t2_4000"
+OUTPUT_DIR = "/scratch/prj/bayunet/experiments/output_vae_hcp_t1_4000"
 AUGMENTATION=False
-INPUT_MODALITIES=["t2"]
-OUTPUT_MODALITIES=["t2"]
-BATCH_SIZE=3
+INPUT_MODALITIES=["t1"]
+OUTPUT_MODALITIES=["t1"]
+BATCH_SIZE=6
 IN_CHANNELS=1
 OUT_CHANNELS=1
-NUM_EPOCHS=4000
+NUM_EPOCHS=1000
 N_SPLITS=5
 F_MAPS=[16, 32, 64, 128, 256]
-
-VAE = False
 
 WARMUP_EPOCHS=None
 LR=0.001
