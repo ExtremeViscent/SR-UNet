c1988b17574c565c36b43a16e838cef043dc75e5
ExtremeViscent <limitviscent@gmail.com>
2022-11-30 19:31:02 +0000

    Colossal-AI adaption

---
+++ configs/FV/cai_dbg.py
@@ -0,0 +1,34 @@
+from colossalai.amp import AMP_TYPE
+import os
+
+DATA_DIR = "/media/hdd/HCP_1200"
+OUTPUT_DIR = "/media/hdd/viscent/SR-UNet/experiments/output_cai_hcp_t2_4000"
+AUGMENTATION=False
+INPUT_MODALITIES=["t2"]
+OUTPUT_MODALITIES=["t2"]
+BATCH_SIZE=6
+IN_CHANNELS=1
+OUT_CHANNELS=1
+NUM_EPOCHS=1000
+N_SPLITS=5
+F_MAPS=[16, 32, 64, 128, 256]
+
+WARMUP_EPOCHS=None
+LR=0.001
+NUM_SAMPLES=100
+LATENT_SIZE = 128
+ALPHA = 0.00025
+DOWN_FACTOR = 5
+DATASET='HCP'
+
+NNODES=1
+NGPUS_PER_NODE=1
+WORLD_SIZE=NNODES*NGPUS_PER_NODE
+
+fp16=dict(
+    mode=AMP_TYPE.NAIVE
+)
+
+parallel = dict(
+    data=dict(size=WORLD_SIZE),
+)
--- train_cai.py
+++ train_cai.py
@@ -13,6 +13,7 @@ from colossalai.engine.schedule import (InterleavedPipelineSchedule,
                                         PipelineSchedule)
 from colossalai.logging import disable_existing_loggers, get_dist_logger
 from colossalai.trainer import Trainer, hooks
+from colossalai.utils.checkpointing import save_checkpoint
 from torch.optim.lr_scheduler import CosineAnnealingLR
 
 
@@ -53,38 +54,31 @@ class BetaScheduler():
         self.model.alpha = self.min + (self.max - self.min) * (1 - np.cos(self.current_step / self.cycle_len * np.pi)) / 2
         self.current_step += 1 
 
+class TrainLoggerHook(colossalai.trainer.hooks.BaseHook):
+    def __init__(self, priority: int, TBLogger):
+        super().__init__(priority)
+        self.TBLogger = TBLogger
+    def after_train_iter(self, trainer, output, label, loss):
+        self.TBLogger(phase='train', step=trainer.cur_step,loss=loss)
 
-
-def eval(model, cur_epoch,fold):
-    output_dir = gpc.config.OUTPUT_DIR
-    output_dir=os.path.join(output_dir,'{}'.format(fold))
-    checkpoint_dir = os.path.join(output_dir, 'checkpoints')
-    pred_dir = os.path.join(output_dir, 'preds')
-    if not os.path.exists(checkpoint_dir):
-        os.makedirs(checkpoint_dir)
-    if not os.path.exists(pred_dir):
-        os.makedirs(pred_dir)
-    # ckpt_path = os.path.join(checkpoint_dir, '{}_sd.pth'.format(cur_epoch))
-    # torch.save(dict(state_dict=model.state_dict()),ckpt_path)
-    ckpt_path = os.path.join(checkpoint_dir, '{}.pth'.format(cur_epoch))
-    if cur_epoch % 10 == 0:
-        torch.save(model,ckpt_path)
-    # model = UNet3D(in_channels=gpc.config.IN_CHANNELS,
-    #                 out_channels=gpc.config.OUT_CHANNELS,
-    #                 f_maps=gpc.config.F_MAPS,
-    #                 layer_order='gcr',
-    #                 num_groups=min(1, gpc.config.F_MAPS[0]//2),
-    #                 is_segmentation=False,
-    #                 )
-    # ckpt = torch.load(ckpt_path)
-    # model.load_state_dict(ckpt['state_dict'])
-    with torch.no_grad():
+class EvalHook(colossalai.trainer.hooks.SaveCheckpointHook):
+    def after_test_epoch(self, trainer):
+        cur_epoch = trainer.cur_epoch
+        output_dir = gpc.config.OUTPUT_DIR
+        checkpoint_dir = os.path.join(output_dir, 'checkpoints')
+        pred_dir = os.path.join(output_dir, 'preds')
+        if not os.path.exists(checkpoint_dir):
+            os.makedirs(checkpoint_dir)
+        if not os.path.exists(pred_dir):
+            os.makedirs(pred_dir)
+        ckpt_path = os.path.join(checkpoint_dir, '{}.pth'.format(cur_epoch))
+        if cur_epoch % 10 == 0:
+            save_checkpoint(op.join(checkpoint_dir,'{}.pth'.format(cur_epoch)), cur_epoch, self.model, trainer.engine.optimizer,self._lr_scheduler)
         image = np.load(os.path.join(output_dir, 'val_image.npy'))
         target = np.load(os.path.join(output_dir, 'val_target.npy'))
         image = torch.tensor(image).unsqueeze(0).cuda()
         target = torch.tensor(target).unsqueeze(0).cuda()
-        model.eval()
-        pred = model(image)
+        pred = trainer.predict(image)
         image = image.cpu().detach().numpy().astype(np.float32)
         target = target.cpu().detach().numpy().astype(np.float32)
         pred = pred.cpu().detach().numpy().astype(np.float32)
@@ -124,36 +118,33 @@ def eval(model, cur_epoch,fold):
 def train():
     # Debug: find anormaly 
     torch.autograd.set_detect_anomaly(True)
-
-    # Rewrite this part (TODO)
+    # Initialize Colossal-AI context
     parser = colossalai.get_default_parser()
     parser.add_argument('--from_torch', default=False, action='store_true')
     args = parser.parse_args()
     disable_existing_loggers()
-
-
+    # Use default config and port
     if args.config is None:
         args.config = './config.py'
     port = 11451
     success = False
-    # Find a free port (TODO: delete this part)
+    # Find a free port
     while not success:
         try:
             colossalai.launch(args.config,0,1,'localhost',port)
             success = True
         except:
             port+=1
-
-    # TODO: Get rid of colossialai
+    # Initialize the logger
     logger = get_dist_logger()
-
+    # Create paths
     output_dir = gpc.config.OUTPUT_DIR
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-
     logger.info('Build data loader')
     # For KFold cross validation, not needed for now
     n_splits = gpc.config.N_SPLITS if gpc.config.N_SPLITS is not None else 5
+    # Initialize data loaders
     if gpc.config.DATASET == 'dHCP':
         dataloaders, val_loader = get_synth_dhcp_dataloader(data_dir=gpc.config.DATA_DIR,
                                                             batch_size=gpc.config.BATCH_SIZE,
@@ -184,113 +175,53 @@ def train():
                                                             n_splits=n_splits,
                                                             augmentation=gpc.config.AUGMENTATION,
                                                             down_factor=gpc.config.DOWN_FACTOR,)
-    WARMUP_EPOCHS = getattr(gpc.config, 'WARMUP_EPOCHS', None)
-    for i in range(0, 5):
-        logger.info('Training fold {}'.format(i), ranks=[0])
-        train_loader, test_loader = dataloaders[i]
-        vae = getattr(gpc.config, 'VAE', True)
-        if vae:
-            if getattr(gpc.config, 'CHECKPOINT', None) is not None:
-                model = torch.load(getattr(gpc.config, 'CHECKPOINT', None))
-            else:
-                model = BUNet3D(in_channels=gpc.config.IN_CHANNELS,
-                                out_channels=gpc.config.OUT_CHANNELS,
-                                f_maps=gpc.config.F_MAPS,
-                                layer_order='gcr',
-                                num_groups=min(1, gpc.config.F_MAPS[0]//2),
-                                is_segmentation=False,
-                                latent_size=gpc.config.LATENT_SIZE,
-                                alpha=gpc.config.ALPHA if gpc.config.ALPHA is not None else 0.00025,
-                                augmentation=getattr(gpc.config,'AUGMENTATION',False),
-                                recon_loss_func=getattr(gpc.config,'RECON_LOSS','mse'),
-                                div_loss_func=getattr(gpc.config,'DIV_LOSS','kl'))
-            criterion = model.VAE_loss
-        else:
-            if getattr(gpc.config, 'CHECKPOINT', None) is not None:
-                model = torch.load(getattr(gpc.config, 'CHECKPOINT', None))
-            else:
-                model = UNet3D(in_channels=gpc.config.IN_CHANNELS,
-                                out_channels=gpc.config.OUT_CHANNELS,
-                                f_maps=gpc.config.F_MAPS,
-                                layer_order='gcr',
-                                num_groups=min(1, gpc.config.F_MAPS[0]//2),
-                                is_segmentation=False,
-                                )
-            criterion = nn.MSELoss()
-        if WARMUP_EPOCHS is None and vae:
-            model.enable_fe_loss()
-            logger.info('Using VAE loss')
-        else:
-            logger.info('Using MSE loss')
-        logger.info('Initializing K-Fold', ranks=[0])
-        optim = torch.optim.Adam(
-            model.parameters(),
-            lr=gpc.config.LR,
-            betas=(0.9, 0.999)
-        )
-        lr_scheduler = CosineAnnealingLR(
-            optim, 1000)
-        if vae: 
-            beta_scheduler = BetaScheduler(model)
-        TBLogger = TensorBoardLogger(log_dir = op.join(output_dir,'{}'.format(i), 'tb_logs'),comment='fold_{}'.format(i))
-        val_image,val_target = val_loader.dataset.__getitem__(0)
-        if not os.path.exists(os.path.join(output_dir, '{}'.format(i))):
-            os.makedirs(os.path.join(output_dir, '{}'.format(i)))
-        np.save(os.path.join(output_dir,'{}'.format(i), 'val_image.npy'), val_image)
-        np.save(os.path.join(output_dir,'{}'.format(i), 'val_target.npy'), val_target)
+    
+    train_loader, test_loader = dataloaders[0]
+    # Define model, optimizer and loss
+    model = UNet3D(in_channels=gpc.config.IN_CHANNELS,
+                    out_channels=gpc.config.OUT_CHANNELS,
+                    f_maps=gpc.config.F_MAPS,
+                    layer_order='gcr',
+                    num_groups=min(1, gpc.config.F_MAPS[0]//2),
+                    is_segmentation=False,
+                    )
+    criterion = nn.MSELoss()
+    logger.info('Using MSE loss')
+    optim = torch.optim.Adam(
+        model.parameters(),
+        lr=gpc.config.LR,
+        betas=(0.9, 0.999)
+    )
+    lr_scheduler = CosineAnnealingLR(optim, 1000)
+    # Initialize the trainer
+    engine, train_dataloader, test_dataloader, lr_scheduler = colossalai.initialize(
+        model,
+        optim, 
+        criterion, 
+        train_loader, 
+        test_loader, 
+        lr_scheduler
+    )
+    trainer = Trainer(engine, logger=logger)
+    # Initialize the TB Logger
+    TBLogger = TensorBoardLogger(log_dir = op.join(output_dir, 'tb_logs'))
+    TBHook = TrainLoggerHook(100, TBLogger)
+    # Saving validation images
+    val_image,val_target = val_loader.dataset.__getitem__(0)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    np.save(os.path.join(output_dir, 'val_image.npy'), val_image)
+    np.save(os.path.join(output_dir, 'val_target.npy'), val_target)
 
-        model.cuda()
-        n_step = 0
-        n_step_test = 0
-
-
-        for epoch in range(gpc.config.NUM_EPOCHS):
-            model.train()
-            if WARMUP_EPOCHS is not None and epoch == WARMUP_EPOCHS:
-                model.enable_fe_loss()
-                logger.info('Using VAE loss', ranks=[0])
-            for im,gt in tqdm(train_loader):
-                im=im.cuda()
-                gt=gt.cuda()
-                optim.zero_grad()
-                output = model(im)
-                loss = criterion(output, gt)
-                TBLogger(phase='train', step=n_step,loss=loss,LR=lr_scheduler.get_last_lr()[0])
-                if getattr(model,'div_loss', None) is not None:
-                    TBLogger(phase='train', step=n_step,KL=model.div_loss,MSE=model.recon_loss, beta=beta_scheduler.get_beta(),beta_KLD=beta_scheduler.get_beta()*model.div_loss)
-                try:
-                    loss.backward()
-                    torch.nn.utils.clip_grad_value_(model.parameters(), clip_value=1.0)
-                    optim.step()
-                except Exception as e:
-                    pass
-
-                n_step+=1
-                if vae:
-                    beta_scheduler.step()
-                lr_scheduler.step()
-
-            logger.info('Epoch {}/{}'.format(epoch, gpc.config.NUM_EPOCHS), ranks=[0])
-            logger.info('Train Loss: {:.4f}'.format(loss.item()), ranks=[0])
-            model.eval()
-            with torch.no_grad():
-                for im,gt in tqdm(test_loader):
-                    im=im.cuda()
-                    gt=gt.cuda()
-                    output = model(im)  
-                    loss = criterion(output, gt)
-                    TBLogger(phase='test', step=n_step_test,loss=loss)
-                    if getattr(model,'div_loss', None) is not None:
-                        TBLogger(phase='test', step=n_step_test,KL=model.div_loss,MSE=model.recon_loss)
-                    n_step_test+=1
-            logger.info('epoch:{}/{}'.format(epoch,gpc.config.NUM_EPOCHS), ranks=[0])
-            logger.info('Test loss:{}'.format(loss), ranks=[0])
-            if getattr(model,'div_loss', None) is not None:
-                logger.info('KL:{}'.format(model.div_loss), ranks=[0])
-                logger.info('MSE:{}'.format(model.recon_loss), ranks=[0])
-                logger.info('Average mu:{}'.format(model.enc_mu.mean()), ranks=[0])
-                logger.info('Average logvar:{}'.format(model.enc_logvar.mean()), ranks=[0])
-            eval(model,epoch,i)
+    
+    trainer.fit(train_dataloader, 
+    gpc.config.NUM_EPOCHS, 
+    None, 
+    test_dataloader,
+    1,
+    [TBHook, 
+    EvalHook(101, op.join(output_dir,'checkpoints','best_model.pth'))],
+    True)
 
             
 
